20336943679f1bc5d70bd93c578820f1e18ba9a9
Joel Brewer <joelbrewer01@gmail.com>
2014-05-24 20:42:29 -0500

    add topics page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ GreaterThan::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
 
+  get 'topics' => 'topics#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
